d616741d5d79fd24d3e08224efe2145ee3abb726
regodon-23 <regodongianandrei23@gmail.com>
2025-09-24 14:45:44 +0800

    Add files via upload

---
+++ activity15.py
@@ -0,0 +1,7 @@
+
+firstname = 'Gian Andrei'
+mid_name = 'Romero'
+lname = 'Regodon'
+
+#print("My name is",firstname," ",mid_name," ",lname)
+print(f"My name is {firstname.upper()} {mid_name.upper()} {lname.upper()}")
