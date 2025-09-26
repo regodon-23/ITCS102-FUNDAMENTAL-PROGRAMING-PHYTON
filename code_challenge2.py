4375ed4f32099f07e07c9a9083664e1e7e5c1a22
regodon-23 <regodongianandrei23@gmail.com>
2025-09-26 17:26:02 +0800

    Add files via upload

---
+++ code_challenge2.py
@@ -0,0 +1,24 @@
+n1 =int(input("Enter amount to deposit : "))
+n2 =int(input("Enter the number of 1000 deposit : "))
+n3 =int(input("Enter the number of 500 deposit : "))
+n4 =int(input("Enter the number of 200 deposit : "))
+n5 =int(input("Enter the number of 100 deposit : "))
+n6 =int(input("Enter the number of 50 deposit : "))
+n7 =int(input("Enter the number of 20 deposit : "))
+n8 =int(input("Enter the number of 10 deposit : "))
+n9 =int(input("Enter the number of 5 deposit : "))
+n10 =int(input("Enter the number of 1 deposit : "))
+
+a = n2 * 1000
+b = n3 * 500
+c = n4 * 200
+d = n5 * 100
+e = n6 * 50
+f = n7 * 20
+g = n8 * 10
+h = n9 * 5
+i = n10 * 1
+
+total = a + b + c + d + e + f + g + h + i
+
+print("Total from denominations: ", total) 
