41786943d74a305b2ede0e429ef5a85f1ba8937b
regodon-23 <regodongianandrei23@gmail.com>
2025-09-26 17:31:20 +0800

    Add files via upload

---
+++ code_challenge3.py
@@ -0,0 +1,10 @@
+username = "andrei"
+pwd = "catlover"
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "andrei"and password == "catlover":
+	print("Access Granted")
+else:
+	print("Access Denied")
---
+++ code_challenge4.py
@@ -0,0 +1,6 @@
+number = eval(input("Enter a number: "))
+
+if number % 2 == 0:
+	print("The number is even")
+else:
+	print("The number is odd")
