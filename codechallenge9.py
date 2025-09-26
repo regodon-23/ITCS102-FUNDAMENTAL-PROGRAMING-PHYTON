d32d3d42efd708391e2db973de2fc9272e5cf542
regodon-23 <regodongianandrei23@gmail.com>
2025-09-26 18:02:31 +0800

    Add files via upload

---
+++ codechallenge9.py
@@ -0,0 +1,7 @@
+print("COUNTDOWN TIMER SIMULATOR")
+countdown = int(input("Enter the starting number for countdown: "))
+print("Countdown started: ")
+
+for y in range(countdown, 0, -1):
+	print(y)
+print("Liftoff! ")
