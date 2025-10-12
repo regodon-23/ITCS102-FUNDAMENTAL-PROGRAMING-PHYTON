741c7dd24686e41115e4d6dc0a5794a473a54b11
regodon-23 <regodongianandrei23@gmail.com>
2025-10-12 19:40:54 +0800

    Add files via upload

---
+++ codechallenge13.py
@@ -0,0 +1,40 @@
+print("\t\t\t               *")
+for a in range(1,2,1):
+    for b in range(20,a,-1):
+        print(" ", end=" ")
+    for c in range(0,a,1):
+        print("*", end=' ')
+    for d in range(0,a,1):
+        print("*", end=" ")
+    print()
+for e in range(3,1,-1):
+        for f in range(e,21,1):
+            print(" ", end=' ')
+        for g in range(e,1,-1):
+            print("*", end=' ')
+        for h in range(e,1,-1):
+            print("*", end=' ')
+        print()
+print("\t\t\t               *")
+for a in range(1,11,1):
+    for b in range(20,a,-1):
+        print(" ", end=" ")
+    for c in range(0,a,1):
+        print("*", end=' ')
+    for d in range(0,a,1):
+        print("*", end=" ")
+    print()
+for a in range(1,15,1):
+    for b in range(20,a,-1):
+        print(" ", end=" ")
+    for c in range(0,a,1):
+        print("*", end=' ')
+    for d in range(0,a,1):
+        print("*", end=" ")
+    print()
+for i in range(1,7,1):
+    for x in range(1,18,1):
+        print(" ", end=' ')
+    for y in range(1,7,1):
+        print("*", end=' ')
+    print()
