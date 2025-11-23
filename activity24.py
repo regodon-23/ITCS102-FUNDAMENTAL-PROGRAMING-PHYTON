8996c34d6a9e329887f52a7f905b58ffa41dad12
regodon-23 <regodongianandrei23@gmail.com>
2025-11-23 14:17:11 +0800

    Add files via upload

---
+++ activity24.py
@@ -0,0 +1,24 @@
+
+#creating your own function
+#keyword , def - define
+#the content inside the parenthesis is called
+#CODE RESUABILITY
+
+
+
+
+
+def greeter(name):
+    print(f"Hi {name}, kamusta ka na ? ")
+
+def summation(x):
+    sum = 0
+    for y in range(x, 0, -1):
+        sum += y
+    print(f"The sum of {x} is {sum}") 
+
+summation(15)
+summation(9)
+summation(17)
+   # greeter ("andrei")
+   # greeter ("Gian")
