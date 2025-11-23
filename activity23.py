e25f1d461867db489514691b507de91bd154fd95
regodon-23 <regodongianandrei23@gmail.com>
2025-11-23 14:16:47 +0800

    Add files via upload

---
+++ activity23.py
@@ -0,0 +1,13 @@
+name = True
+artist = []
+while name == True:
+    artist =("Enter ur fav music artist: ").lower()
+    if artist == 'end':
+        print("list ended.")
+        break
+    else:
+        print(f"{artist} has been added to your list")
+    list.append
+    fullname ='Gian Andrei Regodon'
+    for a in fullname:
+        print(f"{a}")    
