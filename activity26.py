d9216822cdaa874b5ca00d8aad00048dae88777c
regodon-23 <regodongianandrei23@gmail.com>
2025-11-30 14:42:50 +0800

    Add files via upload

---
+++ activity26.py
@@ -0,0 +1,4 @@
+programming_language = ['Python', 'JavaScript', 'Java', 'c#', 'Ruby']
+
+programming_language2 = {'madali,': 'Python', 'komplikado': 'c#', 'Pinakamahirap': 'JavaScript', 'matikas': 'Java', 'malikhain': 'Ruby'}
+print(programming_language2['Pinakamahirap'])
